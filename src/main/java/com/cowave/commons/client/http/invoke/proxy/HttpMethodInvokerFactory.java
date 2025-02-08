cowave5/http-client

-> http-client 2.7.5

src/main/java/com/cowave/commons/client/http/invoke/proxy/HttpMethodInvokerFactory.java
@@ -1,0 +1,65 @@
+package com.cowave.commons.client.http.invoke.proxy;
+
+import com.cowave.commons.client.http.request.MultipartRequestFactory;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.event.Level;
+import com.cowave.commons.client.http.HttpExceptionHandler;
+import com.cowave.commons.client.http.HttpInterceptor;
+import com.cowave.commons.client.http.invoke.codec.HttpDecoder;
+import com.cowave.commons.client.http.invoke.codec.HttpEncoder;
+import com.cowave.commons.client.http.invoke.exec.HttpExecutor;
+import com.cowave.commons.client.http.request.Options;
+import com.cowave.commons.client.http.request.meta.HttpMethodMetaParser;
+import com.cowave.commons.client.http.request.meta.HttpMethodMeta;
+import com.cowave.commons.client.http.request.HttpRequestFactory;
+import com.cowave.commons.client.http.request.BodyRequestFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author shanhuiming
+ *
+ */
+@RequiredArgsConstructor
+public class HttpMethodInvokerFactory {
+    private final HttpExecutor httpExecutor;
+    private final HttpMethodMetaParser metaParser;
+    private final HttpEncoder encoder;
+    private final HttpDecoder decoder;
+    private final Options options;
+    private final List<HttpInterceptor> httpInterceptors;
+    private final HttpExceptionHandler exceptionHandler;
+
+    private final boolean ignoreError;
+    private final Level level;
+
+    public Map<String, MethodInvoker> create(ProxyTarget<?> proxyTarget) throws UnsupportedEncodingException {
+
+        List<HttpMethodMeta> metaList = metaParser.parse(proxyTarget.type());
+
+        Map<String, MethodInvoker> result = new LinkedHashMap<>();
+        for (HttpMethodMeta meta : metaList) {
+            HttpRequestFactory httpRequestFactory;
+            if (meta.getMultipartFileIndex() != null
+                    || meta.getMultipartFormIndex() != null || !meta.getMultipartParams().isEmpty()) {
+                // multipart/form-data
+                httpRequestFactory = new MultipartRequestFactory(meta);
+            } else if (meta.getBodyIndex() != null) {
+                // 存在body
+                httpRequestFactory = new BodyRequestFactory(meta, encoder);
+            } else {
+                httpRequestFactory = new HttpRequestFactory(meta);
+            }
+
+            // <methodKey, MethodHandler>
+            result.put(meta.getMethodKey(),
+                    new HttpMethodInvoker(proxyTarget, meta, httpRequestFactory,
+                            httpExecutor, httpInterceptors, options, decoder, level, ignoreError, exceptionHandler));
+        }
+        return result;
+    }
+}

src/main/java/com/cowave/commons/client/http/register/HttpProxyFactory.java
@@ -1,0 +1,88 @@
+package com.cowave.commons.client.http.register;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Objects;
+
+import com.cowave.commons.client.http.HttpInterceptor;
+import com.cowave.commons.client.http.annotation.HttpClient;
+import com.cowave.commons.client.http.invoke.exec.HttpClientExecutor;
+import com.cowave.commons.client.http.invoke.codec.HttpEncoder;
+import com.cowave.commons.client.http.invoke.proxy.ProxyFactory;
+import com.cowave.commons.client.http.invoke.proxy.ProxyFactoryBuilder;
+import com.cowave.commons.client.http.invoke.proxy.ProxyTarget;
+import com.cowave.commons.client.http.request.Options;
+import org.springframework.context.ApplicationContext;
+import com.cowave.commons.client.http.HttpExceptionHandler;
+import com.cowave.commons.client.http.invoke.codec.HttpDecoder;
+import org.springframework.util.StringUtils;
+import org.springframework.util.StringValueResolver;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSocketFactory;
+
+/**
+ *
+ * @author shanhuiming
+ *
+ */
+class HttpProxyFactory {
+
+    private static ApplicationContext applicationContext;
+
+    private static StringValueResolver valueResolver;
+
+    static void setApplicationContext(ApplicationContext applicationContext) {
+        HttpProxyFactory.applicationContext = applicationContext;
+    }
+
+    static void setStringValueResolver(StringValueResolver valueResolver) {
+        HttpProxyFactory.valueResolver = valueResolver;
+    }
+
+    static <T> T newProxy(Class<T> clazz, HttpClient httpClient) throws UnsupportedEncodingException {
+        int retryTimes = getInt(httpClient.retryTimes(), httpClient.retryTimesStr());
+        int retryInterval = getInt(httpClient.retryInterval(), httpClient.retryIntervalStr());
+        int connectTimeout = getInt(httpClient.connectTimeout(), httpClient.connectTimeoutStr());
+        int readTimeout = getInt(httpClient.readTimeout(), httpClient.readTimeoutStr());
+        ProxyFactoryBuilder proxyFactoryBuilder = new ProxyFactoryBuilder(new Options(connectTimeout, readTimeout, retryTimes, retryInterval));
+
+        String[] interceptors = applicationContext.getBeanNamesForType(HttpInterceptor.class);
+        for (String interceptorName : interceptors) {
+            HttpInterceptor httpInterceptor = applicationContext.getBean(interceptorName, HttpInterceptor.class);
+            proxyFactoryBuilder.addHttpInterceptor(httpInterceptor);
+        }
+
+        String[] handlers = applicationContext.getBeanNamesForType(HttpExceptionHandler.class);
+        if (handlers.length > 0) {
+            String handler = handlers[0];
+            HttpExceptionHandler exceptionHandler = applicationContext.getBean(handler, HttpExceptionHandler.class);
+            proxyFactoryBuilder.setExceptionHandler(exceptionHandler);
+        }
+
+        try {
+            // encode / decoder
+            proxyFactoryBuilder.setEncoder((HttpEncoder) httpClient.encoder().newInstance());
+            proxyFactoryBuilder.setDecoder((HttpDecoder) httpClient.decoder().newInstance());
+            // Https设置
+            Class<? extends SSLSocketFactory> sslSocketFactoryClass = httpClient.sslSocketFactory();
+            Class<? extends HostnameVerifier> hostnameVerifierClass = httpClient.hostnameVerifier();
+            SSLSocketFactory SSLSocketFactory = sslSocketFactoryClass.newInstance();
+            HostnameVerifier hostnameVerifier = hostnameVerifierClass.newInstance();
+            proxyFactoryBuilder.setHttpExecutor(new HttpClientExecutor(SSLSocketFactory, hostnameVerifier));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        ProxyFactory proxyFactory = proxyFactoryBuilder.newProxyFactory(httpClient.level(), httpClient.ignoreError());
+        ProxyTarget<T> proxyTarget = new ProxyTarget<>(
+                applicationContext, valueResolver, clazz, httpClient.name(), httpClient.url());
+        return proxyFactory.newProxy(proxyTarget);
+    }
+
+    private static int getInt(int defaultValue, String regex) {
+        if (!StringUtils.hasText(regex)) {
+            return defaultValue;
+        }
+        return Integer.parseInt(Objects.requireNonNull(valueResolver.resolveStringValue(regex)));
+    }
+}

src/main/java/com/cowave/commons/client/http/request/meta/HttpMethodMeta.java
@@ -1,0 +1,37 @@
+package com.cowave.commons.client.http.request.meta;
+
+import com.cowave.commons.client.http.request.HttpRequest;
+import lombok.Data;
+
+import java.lang.reflect.Type;
+import java.util.*;
+
+/**
+ *
+ * @author shanhuiming
+ *
+ */
+@Data
+public class HttpMethodMeta {
+    private final HttpRequest httpRequest = new HttpRequest();
+    private final Map<Integer, Collection<String>> paramIndexName = new LinkedHashMap<>();
+    private final Map<Integer, String> multiParamIndexName = new LinkedHashMap<>();
+    private final List<String> multipartParams = new ArrayList<>();
+    private Integer urlIndex;
+    private Integer bodyIndex;
+    private Integer headerMapIndex;
+    private Integer paramMapIndex;
+    private Integer hostIndex;
+    private Integer multipartFileIndex;
+    private Integer multipartFormIndex;
+    private String multipartFileName;
+    private String multipartFileBoundary;
+    private String methodKey;
+    private boolean paramMapEncoded;
+    private transient Type bodyType;
+    private transient Type returnType;
+    private int connectTimeout = -1;
+    private int readTimeout = -1;
+    private int retryTimes;
+    private int retryInterval;
+}

cowave5/spring-feign

src/main/java/com/cowave/commons/client/http/invoke/proxy/HttpMethodInvokerFactory.java
@@ -1,0 +1,65 @@
+package com.cowave.commons.client.http.invoke.proxy;
+
+import com.cowave.commons.client.http.request.MultipartRequestFactory;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.event.Level;
+import com.cowave.commons.client.http.HttpExceptionHandler;
+import com.cowave.commons.client.http.HttpClientInterceptor;
+import com.cowave.commons.client.http.invoke.codec.HttpDecoder;
+import com.cowave.commons.client.http.invoke.codec.HttpEncoder;
+import com.cowave.commons.client.http.invoke.exec.HttpExecutor;
+import com.cowave.commons.client.http.request.Options;
+import com.cowave.commons.client.http.request.meta.HttpMethodMetaParser;
+import com.cowave.commons.client.http.request.meta.HttpMethodMeta;
+import com.cowave.commons.client.http.request.HttpRequestFactory;
+import com.cowave.commons.client.http.request.BodyRequestFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author shanhuiming
+ *
+ */
+@RequiredArgsConstructor
+public class HttpMethodInvokerFactory {
+    private final HttpExecutor httpExecutor;
+    private final HttpMethodMetaParser metaParser;
+    private final HttpEncoder encoder;
+    private final HttpDecoder decoder;
+    private final Options options;
+    private final List<HttpClientInterceptor> httpClientInterceptors;
+    private final HttpExceptionHandler exceptionHandler;
+
+    private final boolean ignoreError;
+    private final Level level;
+
+    public Map<String, MethodInvoker> create(ProxyTarget<?> proxyTarget) throws UnsupportedEncodingException {
+
+        List<HttpMethodMeta> metaList = metaParser.parse(proxyTarget.type());
+
+        Map<String, MethodInvoker> result = new LinkedHashMap<>();
+        for (HttpMethodMeta meta : metaList) {
+            HttpRequestFactory httpRequestFactory;
+            if (meta.getMultipartFileIndex() != null
+                    || meta.getMultipartFormIndex() != null || !meta.getMultipartParams().isEmpty()) {
+                // multipart/form-data
+                httpRequestFactory = new MultipartRequestFactory(meta);
+            } else if (meta.getBodyIndex() != null) {
+                // 存在body
+                httpRequestFactory = new BodyRequestFactory(meta, encoder);
+            } else {
+                httpRequestFactory = new HttpRequestFactory(meta);
+            }
+
+            // <methodKey, MethodHandler>
+            result.put(meta.getMethodKey(),
+                    new HttpMethodInvoker(proxyTarget, meta, httpRequestFactory,
+                            httpExecutor, httpClientInterceptors, options, decoder, level, ignoreError, exceptionHandler));
+        }
+        return result;
+    }
+}

src/main/java/com/cowave/commons/client/http/register/HttpProxyFactory.java
@@ -1,0 +1,88 @@
+package com.cowave.commons.client.http.register;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Objects;
+
+import com.cowave.commons.client.http.HttpClientInterceptor;
+import com.cowave.commons.client.http.annotation.HttpClient;
+import com.cowave.commons.client.http.invoke.exec.HttpClientExecutor;
+import com.cowave.commons.client.http.invoke.codec.HttpEncoder;
+import com.cowave.commons.client.http.invoke.proxy.ProxyFactory;
+import com.cowave.commons.client.http.invoke.proxy.ProxyFactoryBuilder;
+import com.cowave.commons.client.http.invoke.proxy.ProxyTarget;
+import com.cowave.commons.client.http.request.Options;
+import org.springframework.context.ApplicationContext;
+import com.cowave.commons.client.http.HttpExceptionHandler;
+import com.cowave.commons.client.http.invoke.codec.HttpDecoder;
+import org.springframework.util.StringUtils;
+import org.springframework.util.StringValueResolver;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSocketFactory;
+
+/**
+ *
+ * @author shanhuiming
+ *
+ */
+class HttpProxyFactory {
+
+    private static ApplicationContext applicationContext;
+
+    private static StringValueResolver valueResolver;
+
+    static void setApplicationContext(ApplicationContext applicationContext) {
+        HttpProxyFactory.applicationContext = applicationContext;
+    }
+
+    static void setStringValueResolver(StringValueResolver valueResolver) {
+        HttpProxyFactory.valueResolver = valueResolver;
+    }
+
+    static <T> T newProxy(Class<T> clazz, HttpClient httpClient) throws UnsupportedEncodingException {
+        int retryTimes = getInt(httpClient.retryTimes(), httpClient.retryTimesStr());
+        int retryInterval = getInt(httpClient.retryInterval(), httpClient.retryIntervalStr());
+        int connectTimeout = getInt(httpClient.connectTimeout(), httpClient.connectTimeoutStr());
+        int readTimeout = getInt(httpClient.readTimeout(), httpClient.readTimeoutStr());
+        ProxyFactoryBuilder proxyFactoryBuilder = new ProxyFactoryBuilder(new Options(connectTimeout, readTimeout, retryTimes, retryInterval));
+
+        String[] interceptors = applicationContext.getBeanNamesForType(HttpClientInterceptor.class);
+        for (String interceptorName : interceptors) {
+            HttpClientInterceptor httpClientInterceptor = applicationContext.getBean(interceptorName, HttpClientInterceptor.class);
+            proxyFactoryBuilder.addHttpInterceptor(httpClientInterceptor);
+        }
+
+        String[] handlers = applicationContext.getBeanNamesForType(HttpExceptionHandler.class);
+        if (handlers.length > 0) {
+            String handler = handlers[0];
+            HttpExceptionHandler exceptionHandler = applicationContext.getBean(handler, HttpExceptionHandler.class);
+            proxyFactoryBuilder.setExceptionHandler(exceptionHandler);
+        }
+
+        try {
+            // encode / decoder
+            proxyFactoryBuilder.setEncoder((HttpEncoder) httpClient.encoder().newInstance());
+            proxyFactoryBuilder.setDecoder((HttpDecoder) httpClient.decoder().newInstance());
+            // Https设置
+            Class<? extends SSLSocketFactory> sslSocketFactoryClass = httpClient.sslSocketFactory();
+            Class<? extends HostnameVerifier> hostnameVerifierClass = httpClient.hostnameVerifier();
+            SSLSocketFactory SSLSocketFactory = sslSocketFactoryClass.newInstance();
+            HostnameVerifier hostnameVerifier = hostnameVerifierClass.newInstance();
+            proxyFactoryBuilder.setHttpExecutor(new HttpClientExecutor(SSLSocketFactory, hostnameVerifier));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        ProxyFactory proxyFactory = proxyFactoryBuilder.newProxyFactory(httpClient.level(), httpClient.ignoreError());
+        ProxyTarget<T> proxyTarget = new ProxyTarget<>(
+                applicationContext, valueResolver, clazz, httpClient.name(), httpClient.url());
+        return proxyFactory.newProxy(proxyTarget);
+    }
+
+    private static int getInt(int defaultValue, String regex) {
+        if (!StringUtils.hasText(regex)) {
+            return defaultValue;
+        }
+        return Integer.parseInt(Objects.requireNonNull(valueResolver.resolveStringValue(regex)));
+    }
+}

src/main/java/com/cowave/commons/client/http/request/meta/HttpMethodMeta.java
@@ -1,0 +1,39 @@
+package com.cowave.commons.client.http.request.meta;
+
+import com.cowave.commons.client.http.request.HttpRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.lang.reflect.Type;
+import java.util.*;
+
+/**
+ *
+ * @author shanhuiming
+ *
+ */
+@Getter
+@Setter
+public class HttpMethodMeta {
+    private final HttpRequest httpRequest = new HttpRequest();
+    private final Map<Integer, Collection<String>> paramIndexName = new LinkedHashMap<>();
+    private final Map<Integer, String> multiParamIndexName = new LinkedHashMap<>();
+    private final List<String> multipartParams = new ArrayList<>();
+    private Integer urlIndex;
+    private Integer bodyIndex;
+    private Integer headerMapIndex;
+    private Integer paramMapIndex;
+    private Integer hostIndex;
+    private Integer multipartFileIndex;
+    private Integer multipartFormIndex;
+    private String multipartFileName;
+    private String multipartFileBoundary;
+    private String methodKey;
+    private boolean paramMapEncoded;
+    private transient Type bodyType;
+    private transient Type returnType;
+    private int connectTimeout = -1;
+    private int readTimeout = -1;
+    private int retryTimes;
+    private int retryInterval;
+}